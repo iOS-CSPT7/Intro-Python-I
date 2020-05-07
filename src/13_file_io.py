68cfa750e456837836c19955ec01c7c85e27d1e2
Stephanie <sbowl001@fiu.edu>
2020-05-07 19:17:33 +0800

    13 in progress

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,6 +8,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
+fp = open("foo.txt")
+
+for line in fp:
+    print(line)
+
+
+fp.close()
 
 # YOUR CODE HERE
 
@@ -15,5 +22,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
+fp = open("bar.txt", "w")
 
+fp.write("""Line 
+Line hi
+Line bye""")
 # YOUR CODE HERE
+fp.close()
