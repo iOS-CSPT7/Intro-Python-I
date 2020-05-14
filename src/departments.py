9bc51da94cb4f3443ef804cf6a2ba54fd75c8de6
Stephanie <sbowl001@fiu.edu>
2020-05-14 23:56:09 +0800

    lecture

--- src/departments.py
+++ src/departments.py
@@ -1,7 +1,13 @@
 class Department: 
-    def __init__(self, name):
+    def __init__(self, name, products =[]):
         self.name = name 
+        self.products = products 
 
 
     def __str__(self):
-        return self.name 
+        output = "" 
+        output += self.name + "\n"
+
+        for index, product in enumerate(self.products): 
+            output += str(index + 1) + ". " + str(product) + "\n"
+        return output 
--- src/store.py
+++ src/store.py
@@ -16,26 +16,27 @@ optional extra attributes:
 - store capacity 
 """
 #composition: a "has-a" relationship
+#Aggregation: a "has-a " relationship
 #polymorphism : works on different data structures
 
 from departments import Department
-
+from products import Clothing, Swords 
 
 class Store: 
-    def __init__(self, name, departments): 
+    def __init__(self, name, departments = []): 
         self.name = name 
-        self. departments = []
+        self. departments = departments 
 
-        for dep in departments:
-            deparment = Department(dep)
-            self.departments.append(deparment)
+        # for dep in departments:
+        #     deparment = Department(dep)
+        #     self.departments.append(deparment)
 
     def __str__(self):
         output = ""
         output += self.name + "\n"
 
         for index, department in enumerate(self.departments):
-            output += str(index + 1) + ". " + str(department) + "\n"
+            output += str(index + 1) + ". " + str(department.name) + "\n"
 
         output += str(len(self.departments) + 1) + ". Exit"
         return output 
@@ -44,16 +45,18 @@ class Store:
     # def __repr__(self): 
     #     return f'Store name is {self.name}'   #can be used for debugging 
 
-store = Store("The Dugout", ["Running", "Baseball", "Basketball", "Fencing"])
+# store = Store("Empty store")
+
+running_products = [Clothing("shorts", 45, "S", "Black"), Clothing("sweat band", 12, "L", "red")]
+fencing_products = [Swords("eppe", 75, 10), Swords("Saber", 75, 5)]
+# store = Store("The Dugout", [Department("Running", running_products), Department("Baseball"), Department("Basketball"), Department("Fencing"), fencing_products])
+dugout_departments = [Department("Running", running_products),
+                      Department("Baseball"),
+                      Department("Basketball"),
+                      Department("Fencing", fencing_products)]
+store = Store("The Dugout", dugout_departments)
 
 print(store)
-
-
-
-
-
- 
-
 
 selection = 0
 while selection != len(store.departments) + 1:
@@ -62,8 +65,10 @@ while selection != len(store.departments) + 1:
         selection = int(selection)
         if selection >= 1 and selection < len(store.departments) + 1:
             print(f"the user selected: {selection}")
+            print(store.departments[selection -1])
+        elif selection == len(store.departments) + 1:
+            print("Thank you for shopping with us today!")
         else:
             print("Choose from the given choices")
     except ValueError:
         print("Choose a number, not an empty string or a letter")
-print("Thank you for shopping with us today!")
